e440c56ac8cf542bc50abf0324ff03bf5e236856
Gitakash912 <25113cn194@glbitm.ac.in>
2026-02-11 12:14:31 +0530

    Add files via upload

---
+++ greatest.c
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+
+int main() {
+    int a,b;
+    printf("Enter a : ");
+    scanf("%d",&a);
+    printf("Enter b : ");
+    scanf("%d",&b);
+
+    if(a>b) {
+        printf("%d is greatest",a);
+    }
+    else {
+        printf("%d is greatest",b);
+    }
+    return 0;
+
+}
